6974daca7d17380ac1d1e28c4fee24f36fc86f6d
kalu6090 <kalu6090@colorado.edu>
2023-07-12 23:48:49 -0600

    added data binding to recycler views

--- WeatherForecastApp/app/src/main/java/com/katy/weatherforecastapp/adapter/DayForecastAdapter.kt
+++ WeatherForecastApp/app/src/main/java/com/katy/weatherforecastapp/adapter/DayForecastAdapter.kt
@@ -17,10 +17,10 @@ class DayForecastAdapter(
     private val dataList: List<List<WeatherData>>
 ) : RecyclerView.Adapter<DayForecastAdapter.ViewHolder>() {
 
-    val utils = RecyclerViewAdapterUtils()
-    private lateinit var binding: DayForecastCardBinding
+    private val utils = RecyclerViewAdapterUtils()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        binding = DayForecastCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding =
+            DayForecastCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
@@ -33,20 +33,29 @@ class DayForecastAdapter(
         val day = dataList[position]
         val data = utils.getMiddleOrFirstTime(day)
         data?.let {
-            utils.setWeatherImage(binding.weatherImage, it.weather.icon)
-            binding.dateText.text = utils.formatDate(it.dtTxt)?.let { str -> context.getString(str) }
-                ?: it.dtTxt.dayOfWeek.toString().capitalize()
-            binding.tempText.text = context.getString(R.string.temp_format, it.main.temp.toInt())
+            utils.setWeatherImage(holder.binding.weatherImage, it.weather.icon)
+            holder.binding.dateText.text =
+                utils.formatDate(it.dtTxt)?.let { str -> context.getString(str) }
+                    ?: it.dtTxt.dayOfWeek.toString().capitalize()
+            holder.binding.tempText.text =
+                context.getString(R.string.temp_format, it.main.temp.toInt())
             val tempIconColor = utils.getTempIconColor(data.main.temp)
-            binding.tempIcon.clearColorFilter()
-            tempIconColor?.let {color -> binding.tempIcon.setColorFilter( ContextCompat.getColor(context, color))}
-            binding.windText.text =
+            holder.binding.tempIcon.clearColorFilter()
+            tempIconColor?.let { color ->
+                holder.binding.tempIcon.setColorFilter(
+                    ContextCompat.getColor(
+                        context,
+                        color
+                    )
+                )
+            }
+            holder.binding.windText.text =
                 context.getString(R.string.wind_text_format_mph, it.wind.speed.toInt())
         }
         holder.itemView.setOnClickListener { onClick(day, holder.itemView) }
     }
 
-    class ViewHolder(private val binding: DayForecastCardBinding) : RecyclerView.ViewHolder(binding.root)
+    class ViewHolder(val binding: DayForecastCardBinding) : RecyclerView.ViewHolder(binding.root)
 
     fun onClick(dayData: List<WeatherData>, view: View) {
         val action =
