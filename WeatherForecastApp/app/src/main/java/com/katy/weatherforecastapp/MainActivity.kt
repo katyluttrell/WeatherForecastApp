e122d90cdbe030dc78937717034008842f3f4a76
kalu6090 <kalu6090@colorado.edu>
2023-07-12 23:10:40 -0600

    Converted main activity to data binding

--- WeatherForecastApp/app/src/main/java/com/katy/weatherforecastapp/MainActivity.kt
+++ WeatherForecastApp/app/src/main/java/com/katy/weatherforecastapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.katy.weatherforecastapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.katy.weatherforecastapp.databinding.ActivityMainBinding
 import com.katy.weatherforecastapp.ui.MainFragment
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -9,6 +11,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
